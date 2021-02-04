e63ef8f104b88bdeb4fc441492e3cc4f3bfa58be
MonkeyPotatoe <enrico.bonadiman.sk@gmail.com>
2021-02-04 15:59:03 +0000

    Update DAY3.js
    Day 3 homework

--- DAY3.js
+++ DAY3.js
@@ -62,14 +62,25 @@ Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
+const strivify = function ('') {
+if ('' ) {
+return ('')
+}
+else {
+return ("Strive" + '')
+}
+}
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
 
-/* WRITE YOUR CODE HERE */
+const check3and7 = function (x) {
+    
+}
+
+}
 
 /* EXERCISE 7
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
