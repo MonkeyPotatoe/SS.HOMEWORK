6222ec1cfa5d25fc0d5f10cf3a3aa7dcca189c19
MonkeyPotatoe <enrico.bonadiman.sk@gmail.com>
2021-02-08 14:40:20 +0000

    update
    last exercise

---
+++ JS/DAY5.js
@@ -0,0 +1,22 @@
+
+
+const changeColor = () => {
+let h1 = document.getElementsByTagName('h1')[0]
+h1.style.color = 'red'
+}
+changeColor()
+
+const changeBackgroundcolor = () => {
+let background = document.getElementsBytagName('body')[0]
+background.style.backgroundcolor = 'grey'
+}
+changeBackgroundcolor()
+
+const changeFooter = () => {
+let footer = document.get
+
+}
+
+
+
+
